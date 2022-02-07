039e47b76b4f78717abde8abddf228f5735bb920
Sean Jones <sean__jones@hotmail.com>
2022-02-07 21:48:31 +0000

    Changes & building delivery component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Header from "./components/header.js"
 import Footer from "./components/footer.js"
+import Bread from './components/sub-components/bread';
 import "./App.css";
 import React, { useState } from 'react';
 import { FaCopyright } from 'react-icons/fa';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className='main-container'>
       <Header/>
+      <Bread/>
       <Footer className="footer"/>
     </div>
   );
--- src/components/sub-components/bread.js
+++ src/components/sub-components/bread.js
@@ -1,13 +1,68 @@
-import "../App.css";
-import React, { useState } from 'react';
+import "../../App.css";
+import React, { useEffect, useState } from 'react';
 
 function Bread() {
+    /*pallet count*/
     const target = 10
-    
+    const [delPallets, setDelPallets] = useState(0);
+    const [bsPallets, setBsPallets] = useState(0);
+    const [totalPallets,setTotalPallets] = useState(0);
+    const calcPallets = () => {
+        setTotalPallets(bsPallets + delPallets)
+    }
+    /*Employee count*/
+    let 
+
+    const [employees,setEmployees] = useState(0);
+    const updateEmployees = ()=> {
+        let counter = 0
+        setEmployees(0)
+        const employeeList = document.getElementById("employee-list")
+        const employeeInputs = employeeList.getElementsByTagName("input")
+
+        for(let i = 0; i < employeeInputs.length; i++){
+            let currentInput = employeeInputs[i]
+
+            console.log(currentInput)
+        }
+        setEmployees(counter)
+    }
+
+
+    useEffect(()=> {
+        calcPallets()
+        updateEmployees()
+    },)
+
     return (
-        <div >
+        <div className="delivery-container">
+            <h2 className="delivery-header">Bread Delivery</h2>
+            
+            <h4 className="component-h4">Please enter delivery quantity</h4>
+            <div className="d-f-row jc-space-e">
+                <section className="d-f-col">
+                    <label className="label-input">Delivery</label>
+                    <input id="del-input"type="text" className="input-field pallet-input"onChange={(e)=> setDelPallets(parseInt(e.target.value))}></input>
+                </section>
+                <section className="d-f-col">
+                <label className="label-input">Backstock</label>
+                    <input id="bs-input"type="text" className="input-field" onChange={(e)=> setBsPallets(parseInt(e.target.value))}></input>
+                </section>
+            </div>
+            
+            <h4 className="component-h4">Please enter team members</h4>
+            <div className="d-f-row jc-space-e" id="employee-list">
+                <input type="text" className="input-field" onChange={() => updateEmployees()}></input>
+                <input type="text" className="input-field" onChange={() => updateEmployees()}></input>
+                <input type="text" className="input-field" onChange={() => updateEmployees()}></input>
+                <input type="text" className="input-field" onChange={() => updateEmployees()}></input>
+            </div>
+            
+            <div>
+                <h3 className="output-text">{totalPallets} pallets should take {employees} team members 10 minutes to complete.</h3>
+            </div>
         </div>
     );
 }
 
-export default Footer;
+export default Bread;
