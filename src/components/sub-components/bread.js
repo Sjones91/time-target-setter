6a9a8176a1846822695677174f27d6c9184a5d8f
Sean Jones <sean__jones@hotmail.com>
2022-02-12 21:59:20 +0000

    version 1.0 final

--- src/components/sub-components/bread.js
+++ src/components/sub-components/bread.js
@@ -28,42 +28,68 @@ function Delivery(props) {
         setTarget(finalTarget)
     }
     /*check fields populated and produce*/
-    const [display] = `${totalPallets} pallets should take ${employees} team members ${target} minutes to complete.`
+    const display = `${totalPallets} pallets should take ${employees} team members ${target} minutes to complete.`
+    const [message, setMessage] = useState(false)
     const checkFields = ()=> {
-        
+        let palletsField = false;
+        let membersField = false;
+
+        if( totalPallets >= 1) {
+            palletsField = true;
+        };
+        if(employees >= 1) {
+            membersField =true;
+        };
+        if(palletsField === true && membersField === true) {
+            setMessage(true)
+        } else {
+            setMessage(false)
+        }
+    }
+    /**Reset form */
+    const resetInputs = ()=> {
+        setEmployees(0);
+        setTotalPallets(0);
+        teamMembers = [];
+        const inputs = document.getElementsByTagName("input");
+        for(let i = 0; i < inputs.length; i++) {
+            if(inputs[i].type == "text"){
+                inputs[i].value = "";
+            }
+        }
     }
 
     /*Use Effect*/
     useEffect(()=> {
         calcPallets()
         calcTarget()
+        checkFields()
     },)
 
     return (
         <div className="delivery-container">
             <h2 className="delivery-header">{title} Delivery</h2>
             <h4>Target: {timeTarget} Minutes</h4>
-            <h4 className="component-h4">Please enter delivery quantity</h4>
             <div className="d-f-row jc-space-e">
                 <section className="d-f-col">
                     <label className="label-input">Delivery</label>
-                    <input id="del-input"type="text" className="input-field pallet-input"onChange={(e)=> setDelPallets(parseInt(e.target.value))}></input>
+                    <input id="del-input"type="text" className="input-field pallet-input"onChange={(e)=> setDelPallets(parseFloat(e.target.value))}></input>
                 </section>
                 <section className="d-f-col">
                 <label className="label-input">Backstock</label>
-                    <input id="bs-input"type="text" className="input-field" onChange={(e)=> setBsPallets(parseInt(e.target.value))}></input>
+                    <input id="bs-input"type="text" className="input-field" onChange={(e)=> setBsPallets(parseFloat(e.target.value))}></input>
                 </section>
             </div>
-            
-            <h4 className="component-h4">Please enter team members</h4>
+            <h4 className="component-h4">Team Members</h4>
             <div className="employeeInput d-f-row jc-space-e" id="employee-list">
                 <input type="text" className="input-field" onChange={(e) => {teamMembers[0] = e.target.value; updateEmployees()}}></input>
                 <input type="text" className="input-field" onChange={(e) => {teamMembers[1] = e.target.value; updateEmployees()}}></input>
                 <input type="text" className="input-field" onChange={(e) => {teamMembers[2] = e.target.value; updateEmployees()}}></input>
                 <input type="text" className="input-field" onChange={(e) => {teamMembers[3] = e.target.value; updateEmployees()}}></input>
             </div>
+            <button type="submit" className="btn" onClick={resetInputs}>Reset form</button>
             <div>
-                <h4 className="output-text">{totalPallets} pallets should take {employees} team members {target} minutes to complete.</h4>
+                <h4 className="output-text">{message? display : "Please enter workload and employees"}</h4>
             </div>
         </div>
     );
